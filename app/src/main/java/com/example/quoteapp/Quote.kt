9133be0f665df35c2593845779fd77f88f3bdbee
Swyom <pswayamsanjog0@gmail.com>
2025-04-09 11:28:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quoteapp/Quote.kt
@@ -0,0 +1,5 @@
+package com.example.quoteapp
+
+import org.w3c.dom.Text
+
+data class Quote( val text: String, val author : String)
---
+++ app/src/main/java/com/example/quoteapp/SplashActivity2.kt
@@ -0,0 +1,34 @@
+package com.example.quoteapp
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity2 : AppCompatActivity() {
+
+    private val splashDelay: Long = 3000 //
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash2)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Navigate to MainActivity after delay
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashDelay)
+    }
+}
